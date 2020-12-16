8b743da3bdeab9d15ead8cc5c0b60e3ea251386e
neemiasbsilva <neemiasbsilva@gmail.com>
2020-12-16 09:17:48 -0400

    preprocessing audio data: load waveform

--- preprocessing_audio_data/prep.py
+++ preprocessing_audio_data/prep.py
@@ -1,11 +1,22 @@
 import librosa, librosa.display
 import matplotlib.pyplot as plt
 
-file_path = "../blues.00000.wav"
-
 # waveform
-signal, sr = librosa.load(file_path, sr=22050) # sr * T -> 22050 * 30
 
+def load_waveform(file_path):
+    signal, sr = librosa.load(file_path, sr=22050) # sr * T -> 22050 * 30
+
+    librosa.display.waveplot(signal, sr=sr)
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.show()
+    return signal, sr
+
+if __name__ == "__main__":
+
+    file_path = "./blues.00000.wav"
+
+    signal, sr = load_waveform(file_path)
 
 
 
