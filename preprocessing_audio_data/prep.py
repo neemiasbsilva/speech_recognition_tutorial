47434b5148a6353bdff1da91d507109db090f96d
neemiasbsilva <neemiasbsilva@gmail.com>
2020-12-16 09:08:26 -0400

    preprocessing audio data

---
+++ preprocessing_audio_data/prep.py
@@ -0,0 +1,12 @@
+import librosa, librosa.display
+import matplotlib.pyplot as plt
+
+file_path = "../blues.00000.wav"
+
+# waveform
+signal, sr = librosa.load(file_path, sr=22050) # sr * T -> 22050 * 30
+
+
+
+
+
